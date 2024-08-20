fdd122c9998a6c583c13504d38e74dea5c7dadc1
sahillchalkhure26 <sahil.chalkhure@ucdconnect.ie>
2024-08-20 17:10:21 +0100

    made changes to the file for betterment with more samples

--- src/config.py
+++ src/config.py
@@ -1,19 +1,17 @@
-# Import necessary packages/dependencies
 import os
 
 # Directory paths
-BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Absolute path of base directory
-DATASET_PATH = os.path.join(BASE_PATH, 'dataset')  # Path to the dataset directory
-LABELS_PATH = os.path.join(BASE_PATH, 'labels.txt')  # Path to the labels directory
-ARTIFACTS_PATH = os.path.join(BASE_PATH, 'artifacts')  # New path for storing pickle files and models
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Absolute path of base directory
+DATASET_DIR = os.path.join(BASE_DIR, 'dataset')  # Path to the dataset directory
+LABELS_FILE = os.path.join(BASE_DIR, 'labels.txt')  # Path to the labels directory
+ARTIFACTS_DIR = os.path.join(BASE_DIR, 'artifacts')  # Path to the directory for storing artifacts like models and data
 
 # Constants for data collection
 NUM_SAMPLES = 50  # Number of samples to collect for each label
 
 # Model configuration
-# MODEL_NAME = 'model.keras'
-MODEL_NAME = 'model.pkl'
-DATA_PICKLE_NAME = 'data.pickle'
+MODEL_FILENAME = 'model.pkl'    # Filename for the serialized model
+DATA_PICKLE_FILENAME = 'data.pickle'    # Filename for the dataset pickle file
 
 # Logging configuration
 LOGGING_LEVEL = 'INFO'
